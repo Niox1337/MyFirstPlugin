78a17b57dec095251a21d2520287734a5830c99a
Zhixiang Feng <2616718f@student.gla.ac.uk>
2024-04-06 16:31:07 +0100

    enable and disable messages

--- src/main/java/lol/niox/myfirstplugin/MyFirstPlugin.java
+++ src/main/java/lol/niox/myfirstplugin/MyFirstPlugin.java
@@ -7,11 +7,12 @@ public final class MyFirstPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Hello, Spigot!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Goodbye, Spigot!");
     }
 }
